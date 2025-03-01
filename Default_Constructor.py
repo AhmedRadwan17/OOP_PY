5e2b2b14f4aaa72dd8fcf8879e9b6f26caf0f619
Ahmed Radwan <marimradwan922@gmail.com>
2025-03-02 01:15:30 +0200

    Default_Constructor.py

--- Default_Constructor.py
+++ Default_Constructor.py
@@ -3,4 +3,7 @@ class member:
     # Constructor 
     def __init__(self):
         print("Added Successfully")
-member()
+    #(OutPut) => Added Successfully
+one = member()
+print(one.__class__)
+# (OutPut) => <class '__main__.member'>
