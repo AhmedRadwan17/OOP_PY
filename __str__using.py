b4ae923d6e63131ec08b0a4ae95b63514e47d075
Ahmed Radwan <marimradwan922@gmail.com>
2025-03-02 22:22:39 +0200

    __str__using.py

---
+++ __str__using.py
@@ -0,0 +1,14 @@
+class member:
+    def __init__(self):
+        self.skills = ["HTML","CPP","JAVA"]
+    def __str__(self):
+        return f"My Skills are: {self.skills}"
+Man1=member()
+print(Man1)
+
+"""
+--------------------------------------
+    Without Using __str__ The Output Will Print => <__main__.member object at 0x7b44506a1400>
+    When You Using __str__ The OutPut Will Print My Skills are: ['HTML', 'CPP', 'JAVA']
+--------------------------------------
+"""
