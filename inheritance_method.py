b5899c0ddeba571abf30eaa454b637f218f40a9a
Ahmed Radwan <marimradwan922@gmail.com>
2025-03-03 03:10:27 +0200

    inheritance_method.py

---
+++ inheritance_method.py
@@ -0,0 +1,10 @@
+class eat:
+    def init(self):
+        print("Hello everyone")
+    def Say(self):
+        print("Hello")
+class food(eat):
+    def init(self):
+        print("Hello Ramy")
+one = food()
+one.Say()
